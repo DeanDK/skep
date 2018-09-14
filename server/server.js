1db6df8aa0b67dd83b24db2cbd237bef1ea5b6a5
DeanDK <deanbozic12@gmail.com>
2018-09-14 15:02:24 +0200

    mongoose setup and added user model

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const config = require("./config/config").get(process.env.NODE_ENV);
 const mongoose = require("mongoose");
 
 const app = express();
 
 // mongoose
+mongoose.Promise = global.Promise;
+mongose.connect(config.DATABASE);
 
 // executed every time when app receives req
 app.use(bodyParser.json());
