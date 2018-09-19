0e8d848a5113587857c216780f5ec5e465fd1e9a
DeanDK <deanbozic12@gmail.com>
2018-09-19 23:00:36 +0200

    added bars and font-awesome

---
+++ client/src/hoc/layout.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Bar = ({}) => <FontAwesomeIcon id="bar" icon="bars" sixe="2x" />;
+
+export default Bar;
--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,11 @@ import ReduxThunk from "redux-thunk";
 import reducers from "./reducers";
 import Routes from "./routes";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faBars);
 const createStoreWithMiddleware = applyMiddleware(
   promiseMiddleware,
   ReduxThunk
