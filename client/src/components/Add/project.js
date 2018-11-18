0c097afcceacc81709ca0fbd0472d6df8343c545
DeanDK <deanbozic12@gmail.com>
2018-11-18 19:26:05 +0100

    implemented state on toggle to change when between project or internship form

--- client/src/components/Add/project.js
+++ client/src/components/Add/project.js
@@ -16,7 +16,8 @@ class Project extends Component {
     projectName: "",
     grade: "",
     study: "",
-    subject: ""
+    subject: "",
+    projectOrInternship: true
   };
 
   _handleChange = e => {
@@ -58,83 +59,164 @@ class Project extends Component {
     storage.ref(`${email}/${projectName}`).put(file);
   };
 
+  _handleToggle = e =>
+    this.setState({ projectOrInternship: !this.state.projectOrInternship });
+
   render() {
-    return (
-      <div className="container">
-        <div className="sideText">
-          <label id="legend" className="fas fa-paper-plane">
-            <FontAwesomeIcon icon="paper-plane" id="paper-icon" />
-            SHARE
-          </label>
-          <label id="share">Share Your files</label>
-          <label id="safe">Safe. Secure. Free</label>
-        </div>
-        <div className="form-style-5">
-          <div className="toggle">
-            <label className="switch">
-              <input
-                id="preenty-switch"
-                type="checkbox"
-                v-model="preenty_switch"
-              />
-              <span className="slider round" />
+    if (this.state.projectOrInternship) {
+      return (
+        <div className="container">
+          <div className="sideText">
+            <label id="legend" className="fas fa-paper-plane">
+              <FontAwesomeIcon icon="paper-plane" id="paper-icon" />
+              SHARE
             </label>
-            <div className="fas fa-lock"> Upload a project</div>
+            <label id="share">Share Your files</label>
+            <label id="safe">Safe. Secure. Free</label>
           </div>
-          <input
-            type="text"
-            name="nameField"
-            placeholder="Project Name *"
-            required
-            onChange={this._handleProjectName}
-          />
-          <label htmlFor="semester">Study:</label>
-          <select
-            id="semester"
-            name="semesterSelect"
-            onChange={this._handleStudy}
-          >
-            <option value="ICT">ICT</option>
-            <option value="Marketing">Marketing</option>
-            <option value="VCM">VCM</option>
-            <option value="Civil Engineering">Civil Engineering</option>
-            <option value="Architectural Engineering">
-              Architectural Engineering
-            </option>
-          </select>
-          <label htmlFor="subject">Subject:</label>
-          <select
-            id="subject"
-            name="subjectSelect"
-            onClick={this._handleSubject}
-          >
-            <option value="SDJ">SDJ</option>
-            <option value="CON">CON</option>
-            <option value="SEP">SEP</option>
-          </select>
-          <label htmlFor="grade">Grade:</label>
-          <select id="grade" name="gradeSelect" onClick={this._handleGrade}>
-            <option value="7">7</option>
-            <option value="10">10</option>
-            <option value="12">12</option>
-          </select>
-          <input
-            type="file"
-            name="file"
-            id="file-upload"
-            className="inputfile"
-            onChange={this._handleChange}
-          />
-          <label htmlFor="file-upload">
-            <span className="fas fa-upload" /> Choose a file
-          </label>
-          <div id="file-upload-filename" />
-          <button type="submit" onClick={this._handleUpload}>
-            <i className="fas fa-check" /> Submit
-          </button>
+          <div className="form-style-5">
+            <div className="toggle">
+              <label className="switch">
+                <input
+                  id="preenty-switch"
+                  type="checkbox"
+                  v-model="preenty_switch"
+                  onClick={this._handleToggle}
+                />
+                <span className="slider round" />
+              </label>
+              <div className="fas fa-lock">Upload Project</div>
+            </div>
+            <input
+              type="text"
+              name="nameField"
+              placeholder="Project Name "
+              required
+              onChange={this._handleProjectName}
+            />
+            <label htmlFor="semester">Study:</label>
+            <select
+              id="semester"
+              name="semesterSelect"
+              onChange={this._handleStudy}
+            >
+              <option value="ICT">ICT</option>
+              <option value="Marketing">Marketing</option>
+              <option value="VCM">VCM</option>
+              <option value="Civil Engineering">Civil Engineering</option>
+              <option value="Architectural Engineering">
+                Architectural Engineering
+              </option>
+            </select>
+            <label htmlFor="subject">Subject:</label>
+            <select
+              id="subject"
+              name="subjectSelect"
+              onClick={this._handleSubject}
+            >
+              <option value="SDJ">SDJ</option>
+              <option value="CON">CON</option>
+              <option value="SEP">SEP</option>
+            </select>
+            <label htmlFor="grade">Grade:</label>
+            <select id="grade" name="gradeSelect" onClick={this._handleGrade}>
+              <option value="7">7</option>
+              <option value="10">10</option>
+              <option value="12">12</option>
+            </select>
+            <input
+              type="file"
+              name="file"
+              id="file-upload"
+              className="inputfile"
+              onChange={this._handleChange}
+            />
+            <label htmlFor="file-upload">
+              <span className="fas fa-upload" /> Choose a file
+            </label>
+            <div id="file-upload-filename" />
+            <button type="submit" onClick={this._handleUpload}>
+              <i className="fas fa-check" /> Submit
+            </button>
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
+
+    if (!this.state.projectOrInternship) {
+      return (
+        <div className="container">
+          <div className="sideText">
+            <label id="legend" className="fas fa-paper-plane">
+              <FontAwesomeIcon icon="paper-plane" id="paper-icon" />
+              SHARE
+            </label>
+            <label id="share">Share Your files</label>
+            <label id="safe">Safe. Secure. Free</label>
+          </div>
+          <div className="form-style-5">
+            <div className="toggle">
+              <label className="switch">
+                <input
+                  id="preenty-switch"
+                  type="checkbox"
+                  v-model="preenty_switch"
+                  onClick={this._handleToggle}
+                />
+                <span className="slider round" />
+              </label>
+              <div className="fas fa-lock"> Upload Intern</div>
+            </div>
+            <input
+              type="text"
+              name="nameField"
+              placeholder="Company Name"
+              required
+            />
+            <label htmlFor="semester">Study:</label>
+            <select id="semester" name="semesterSelect">
+              <option value="ICT">ICT</option>
+              <option value="Marketing">Marketing</option>
+              <option value="VCM">VCM</option>
+              <option value="Civil Engineering">Civil Engineering</option>
+              <option value="Architectural Engineering">
+                Architectural Engineering
+              </option>
+            </select>
+            <label htmlFor="subject">Country:</label>
+            <select
+              id="subject"
+              name="subjectSelect"
+              onClick={this._handleSubject}
+            >
+              <option value="SDJ">Denmark</option>
+              <option value="CON">USA</option>
+              <option value="SEP">Germany</option>
+            </select>
+            <label htmlFor="grade">Year of Finishing:</label>
+            <select id="grade" name="gradeSelect" onClick={this._handleGrade}>
+              <option value="7">2018</option>
+              <option value="10">2017</option>
+              <option value="12">2016</option>
+            </select>
+            <input
+              type="file"
+              name="file"
+              id="file-upload"
+              className="inputfile"
+              onChange={this._handleChange}
+            />
+            <label htmlFor="file-upload">
+              <span className="fas fa-upload" /> Choose a file
+            </label>
+            <div id="file-upload-filename" />
+            <button type="submit" onClick={this._handleUpload}>
+              <i className="fas fa-check" /> Submit
+            </button>
+          </div>
+        </div>
+      );
+    }
   }
 }
 
